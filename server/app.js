9e09b155fc0ee80586a3d8ad3e6bcf85238b45ab
manish chandra <manishchandra012@gmail.com>
2020-10-31 11:06:27 +0530

    added gitignore

--- server/app.js
+++ server/app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const formData = require("express-form-data");
+const { MongoClient } = require("mongodb");
 const path = require('path');
 const cors = require('cors');
 const jwt = require ('jsonwebtoken');
-const users = require('./data.js').users;
+
 
 const bcrypt = require('bcrypt');
 const { static } = require('express');
@@ -11,6 +12,8 @@ const saltRounds = 13;
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const uri = "mongodb+srv://manish:bi0hazard@cluster0.qqsro.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {useUnifiedTopology: true});
 
 let options = {
     dotfiles: "ignore", //allow, deny, ignore
@@ -30,91 +33,7 @@ app.use(formData.parse());
 app.use(cors());
 app.use(express.static('client',options));
 
-app.post('/register', async (req, res)=>{
-    const userMatch = users.find((user)=> req.body.username === user.username);
-    if(!userMatch){
-        let passHash = await bcrypt.hash(req.body.password, saltRounds);
-        let newUser = {
-            username : req.body.username,
-            email : req.body.email,
-            password : passHash,
-        }
-        let myObj = {
-            name : req.body.username,
-            status : true,
-            message : '',
-        }
-        users.push(newUser);
-        console.log('Full user list', users);
-        let limit = 60 * 3;
-        let expires = Math.floor(Date.now() / 1000)+ limit;
-        let payload = {
-            username : req.body.username,
-            exp : expires,
-        }
-        let token = jwt.sign(payload, 'abcd');
-        myObj.token = token;
-        res.status(201).send(JSON.stringify(myObj));
-    }else{
-        let myObj = {
-            name : req.body.username,
-            status : false,
-            message : 'username not available',
-        }
-        console.log('username not available');
-        res.status(201).send(JSON.stringify(myObj));
-    }
-    
-});
-
-
-app.post('/login', async (req, res)=>{
-    const userMatch = users.find((user)=> req.body.username === user.username);
-    if(userMatch){
-        let submittedPass = req.body.password;
-        let savedPass = userMatch.password;
-        let passwordDidMatch = await bcrypt.compare(submittedPass, savedPass);
-        if(passwordDidMatch){
-            let myObj = {
-                name : req.body.username,
-                status : true,
-                message : '', 
-            }
-            console.log('Full user list', users);
-            let limit = 60 * 3;
-            let expires = Math.floor(Date.now() / 1000)+ limit;
-            let payload = {
-                username : req.body.username,
-                exp : expires,
-            }
-            let token = jwt.sign(payload, 'abcd');
-            myObj.token = token;
-            res.status(201).send(JSON.stringify(myObj));
-        }else{
-            let myObj = {
-                name : req.body.username,
-                status : false,
-                message : 'wrong username or password', 
-            }
-            console.log('Full user list', users);
-            res.status(401).send(JSON.stringify(myObj));
-
-        }
-
-    }else{
-        let myObj = {
-            name : req.body.username,
-            status : false,
-            message : 'wrong username or password',
-        }
-        console.log('username not matched');
-        res.status(201).send(JSON.stringify(myObj));
-    }
-    
-});
-
-
-app.get('/', (req, res)=>{
+async function authentication(req, res, next){
     const header = req.header('Authorization');
     console.log(header);
     if(header){
@@ -153,10 +72,115 @@ app.get('/', (req, res)=>{
 
     }
 }else{
-
-    res.sendFile(path.resolve('client/index.html'));
+    next();
 }
 
+}
+
+app.post('/register', async (req, res)=>{
+    await client.connect();
+    const database = client.db('user');
+    const collection = database.collection('user');
+       
+    const userMatch = await collection.find({
+        username: req.body.username,
+      });
+
+      console.log(await userMatch.count());
+    if(await userMatch.count() == 0){
+        let passHash = await bcrypt.hash(req.body.password, saltRounds);
+        let newUser = {
+            username : req.body.username,
+            email : req.body.email,
+            password : passHash,
+        }
+        let myObj = {
+            name : req.body.username,
+            status : true,
+            message : '',
+        }
+
+        const result = await collection.insertOne(newUser);
+        console.dir(result.insertedCount);
+        console.log('Full user list');
+        let limit = 60 * 3;
+        let expires = Math.floor(Date.now() / 1000)+ limit;
+        let payload = {
+            username : req.body.username,
+            exp : expires,
+        }
+        let token = jwt.sign(payload, 'abcd');
+        myObj.token = token;
+        res.status(201).send(JSON.stringify(myObj));
+    }else{
+        let myObj = {
+            name : req.body.username,
+            status : false,
+            message : 'username not available',
+        }
+        console.log('username not available');
+        res.status(201).send(JSON.stringify(myObj));
+    }
+    
+});
+
+
+app.post('/login', async (req, res)=>{
+
+    await client.connect();
+    const database = client.db('user');
+    const collection = database.collection('user');
+       
+    const userMatch = await collection.findOne({
+        username: req.body.username,
+      });
+
+    if(await userMatch != null){
+        let submittedPass = req.body.password;
+        let savedPass = userMatch.password;
+        let passwordDidMatch = await bcrypt.compare(submittedPass, savedPass);
+        if(passwordDidMatch){
+            let myObj = {
+                name : req.body.username,
+                status : true,
+                message : '', 
+            }
+            console.log('Full user list');
+            let limit = 60 * 3;
+            let expires = Math.floor(Date.now() / 1000)+ limit;
+            let payload = {
+                username : req.body.username,
+                exp : expires,
+            }
+            let token = jwt.sign(payload, 'abcd');
+            myObj.token = token;
+            res.status(201).send(JSON.stringify(myObj));
+        }else{
+            let myObj = {
+                name : req.body.username,
+                status : false,
+                message : 'wrong username or password', 
+            }
+            console.log('Full user list', users);
+            res.status(401).send(JSON.stringify(myObj));
+
+        }
+
+    }else{
+        let myObj = {
+            name : req.body.username,
+            status : false,
+            message : 'wrong username or password',
+        }
+        console.log('username not matched');
+        res.status(201).send(JSON.stringify(myObj));
+    }
+    
+});
+
+
+app.get('/', authentication, (req, res)=>{
+    res.sendFile(path.resolve('client/index.html'));
 });
 
 
